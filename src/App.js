3ca423c217c8bd4b4568bb4617414bb656d22b87
Yosra-sd <sahnoun.yosraa@gmail.com>
2021-03-20 12:47:17 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -4,24 +4,38 @@
   class App extends React.Component{
 
     state = {Person: 
+
       {fullName: 'Yosra Sahnoun' ,
       bio: 'Etudiante'  ,
       imgSrc: Photo , 
       profession: 'Etudiante',
-    },show:true}
+      }, 
+      show: true,
+      date:''
+    }
 
-    componentDidMount
+    componentDidMount () {
+    setInterval(() => {
+      let ba = new Date()
+        const date = ba.getHours() + ":" +ba.getMinutes();
+        this.setState({date:date})
+      },1000)
+    }
     
     render(){
       return(
         <>
         {
-          
-
-          this.state.show && (<>  <p>Fullname : {this.state.Person.fullName}</p>
-            <p>Biography : {this.state.Person.bio}</p>
-            <img src={this.state.Person.imgSrc} />
-            <p>Profession: {this.state.Person.profession}</p></>)
+          this.state.show && (
+          <>  
+          <p>Fullname : {this.state.Person.fullName}</p>
+          <p>Biography : {this.state.Person.bio}</p>
+          <img src={this.state.Person.imgSrc} />
+          <p>Profession: {this.state.Person.profession}</p>
+          <p>{this.state.date}</p>
+          </>  
+          )
+            
         }
         <button onClick={()=>{this.setState({show:!this.state.show })}}>change</button>
         </>
